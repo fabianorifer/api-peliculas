b50ace867cec579cdd52b388d6a28581814e3921
Luis Fabiano Ivor Rivadeneira Fernandez <166071518+fabianorifer@users.noreply.github.com>
2024-11-17 20:25:34 -0500

    Update CrearPelicula.py

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -5,8 +5,58 @@ import os
 def lambda_handler(event, context):
     # Entrada (json)
     print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
+    try:
+        tenant_id = event['body']['tenant_id']
+    except Exception as exception:
+        error = {
+            "tipo": "ERROR",
+            "log_datos": {
+                "message": "Hubo un error al leer el tenant_id",
+                "error": str(exception)
+            }
+        }
+        print(error)
+        return {
+            'statusCode': 500,
+            "message": "Hubo un error al leer el tenant_id",
+            "error": str(exception)
+        }
+    else:
+        info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "message": "El tenant_id se leyo correctamente",
+                "info": tenant_id
+            }
+        }
+        print(info)
+
+    try:
+        pelicula_datos = event['body']['pelicula_datos']
+    except Exception as exception:
+        error = {
+            "tipo": "ERROR",
+            "log_datos": {
+                "message": "Error al leer pelicula_datos",
+                "error": str(exception)
+            }
+        }
+        print(error)
+        return {
+            'statusCode': 500,
+            "message": "Error al leer pelicula_datos",
+            "error": str(exception)
+        }
+    else:
+        info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "message": "Se leyó pelicula_datos correctamente",
+                "info": pelicula_datos
+            }
+        }
+        print(info)
+
     nombre_tabla = os.environ["TABLE_NAME"]
     # Proceso
     uuidv4 = str(uuid.uuid4())
@@ -15,11 +65,71 @@ def lambda_handler(event, context):
         'uuid': uuidv4,
         'pelicula_datos': pelicula_datos
     }
+
+    info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "message": "Se inserto la pelicula",
+                "info": pelicula
+            }
+        }
+    print(info)
+
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=pelicula)
+
+    try:
+        table = dynamodb.Table(nombre_tabla)
+    except Exception as exception:
+        error = {
+            "tipo": "ERROR",
+            "log_datos": {
+                "message": "Hubo un error al conectarse a la tabla",
+                "error": str(exception)
+            }
+        }
+        print(error)
+        return {
+            'statusCode': 500,
+            'message': "Hubo un error al conectarse a la tabla",
+            'error': str(exception)
+        }
+    else:
+        info = {
+            "tipo": "INFO",
+            "log_datos": {
+                "message": "Conexion a la tabla correctamente",
+            }
+        }
+        print(info)
+    try:
+        response = table.put_item(Item=pelicula)
+    except Exception as exception:
+        error = {
+            "tipo": "ERROR",
+            "log_datos": {
+                "message": "Error al insertar en la tabla",
+                "error": str(exception)
+            }
+        }
+        print(error)
+        return {
+            'statusCode': 500,
+            'message': "Hubo un error al insertar en la tabla",
+            'error': str(exception)
+        }
+
     # Salida (json)
-    print(pelicula) # Log json en CloudWatch
+
+    info = {
+        "tipo": "INFO",
+        "log_datos": {
+            "message": "Se realizó la insercion correctamente",
+            "info": response
+        }
+    }
+
+    print(info) # Log json en CloudWatch
+
     return {
         'statusCode': 200,
         'pelicula': pelicula,
